61b77ca9fd0fe8caadb57a10633a4a21570b1e89
René Ghosh <rene.ghosh@gmail.com>
2021-06-08 13:14:15 +0200

    deletion

--- personal/sheetserver.js
+++ personal/sheetserver.js
@@ -22,29 +22,29 @@ export async function getDatabaseRange(sheetId, sheetName) {
     let databaseValues = response.result.values;
     //max columns and max rows to determine the database range  
     let maxRows = databaseValues.length;
-    let maxColumns = maxRows > 0? databaseValues[0].length:0;
+    let maxColumns = maxRows > 0 ? databaseValues[0].length : 0;
     let trimmedTable = [];
     var newMaxColumns = 0;
-    for (let i=0;i<databaseValues.length;i++){
+    for (let i = 0; i < databaseValues.length; i++) {
         let row = databaseValues[i];
-        if (row.length==0 || row[0]=="")  {
+        if (row.length == 0 || row[0] == "") {
             break;
-        } 
+        }
         if (newMaxColumns == 0) {
-            for (let j=0;j<row.length;j++){
+            for (let j = 0; j < row.length; j++) {
                 if (row[j] != "") {
                     newMaxColumns++;
                 }
             }
         }
-        trimmedTable.push(row.slice(0, newMaxColumns));        
+        trimmedTable.push(row.slice(0, newMaxColumns));
     }
     return trimmedTable;
 }
 
 //get sheet columns
 export function getColumnNames(range) {
-    let numRows = range.length;    
+    let numRows = range.length;
     if (numRows >= 1) {
         //read the first row to determine the column names
         var columnNames = [];
@@ -72,73 +72,87 @@ function valuesAsMap(range) {
     }
     return myMap;
 }
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-function numberToLetter(number){
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+function numberToLetter(number) {
     var left = number;
     var d1 = 0;
-    var d0 = 0;    
-    while (left>0) {
-        if (left>=26) {
+    var d0 = 0;
+    while (left > 0) {
+        if (left >= 26) {
             left -= 26;
             d1++;
         } else {
-          d0 = left;
-          left = 0;  
+            d0 = left;
+            left = 0;
         }
     }
-    return (d1>0?letters[d1-1]:"")+letters[d0-1];
+    return (d1 > 0 ? letters[d1 - 1] : "") + letters[d0 - 1];
 }
 
 export async function add(sheetId, sheetName, values) {
-    
     let range = await getDatabaseRange(sheetId, sheetName);
     let numRows = range.length;
     let columns = getColumnNames(range);
     values[columns[0]] = makeUUID();
-    let updateObject = new Array(Object.keys(values).length+1);
+    let updateObject = new Array(Object.keys(values).length + 1);
     for (let [item, value] of Object.entries(values)) {
         let columnIndex = columns.findIndex(name => name == item);
         if (columnIndex >= 0) {
-            updateObject[columnIndex] = value;            
+            updateObject[columnIndex] = value;
         }
     }
-    // log(sheetName+"!A"+(range.length+1)+":"+numberToLetter(updateObject.length)+(range.length+1));
-    // log(updateObject.length);
     let response = await gapi.client.sheets.spreadsheets.values.update({
         spreadsheetId: sheetId,
-        range: sheetName+"!A"+(range.length+1)+":"+numberToLetter(updateObject.length)+(range.length+1),
+        range: sheetName + "!A" + (range.length + 1) + ":" + numberToLetter(updateObject.length) + (range.length + 1),
         valueInputOption: "USER_ENTERED",
         resource: {
             majorDimension: "ROWS",
             values: [updateObject]
         }
-    });    
+    });
     log(response);
     return 1;
 }
 
-export function update(databaseName, id, values) {
-    let dataRange = getTableByName(databaseName);
-    let numRows = dataRange.getNumRows();
-    let columns = getColumnNames(dataRange);
-    for (let [index, row] of Object.entries(dataRange.getValues())) {
-        if (row[0] == id) {
-            for (let [item, value] of Object.entries(values)) {
-                let columnIndex = columns.findIndex(name => name == item);
-                if (columnIndex >= 0) {
-                    dataRange.getCell(parseInt(index) + 1, columnIndex + 1).setValue(value);
-                }
-            }
+export function update(sheetId, sheetName, id, values) {
+    let range = await getDatabaseRange(sheetId, sheetName);
+    let columns = getColumnNames(range);
+    let updateObject = new Array(Object.keys(values).length + 1);
+    for (let [item, value] of Object.entries(values)) {
+        let columnIndex = columns.findIndex(name => name == item);
+        if (columnIndex >= 0) {
+            updateObject[columnIndex] = value;
         }
     }
+    for (let [index, row] of Object.entries(range)) {
+        if (row[0] == id) {
+            let response = await gapi.client.sheets.spreadsheets.values.update({
+                spreadsheetId: sheetId,
+                range: sheetName + "!A" + (index + 1) + ":" + numberToLetter(updateObject.length) + (index + 1),
+                valueInputOption: "USER_ENTERED",
+                resource: {
+                    majorDimension: "ROWS",
+                    values: [updateObject]
+                }
+            });
+            return 1;
+        }
+    }
+    return 0;
 }
 
 //delete a row entry
 export function deleteEntry(databaseName, id) {
-    let dataRange = getTableByName(databaseName);
-    for (let [index, row] of Object.entries(dataRange.getValues())) {
+    let range = await getDatabaseRange(sheetId, sheetName);
+
+    for (let [index, row] of Object.entries(range)) {
         if (row[0] == id) {
-            let newRange = dataRange.getSheet().deleteRow(parseInt(index) + 1);
+            let response = await gapi.client.sheets.spreadsheets.values.clear({
+                spreadsheetId: sheetId,
+                clearedRange: sheetName + "!A" + (index + 1) + ":" + numberToLetter(updateObject.length) + (index + 1),
+            });
+            log(response);
+
         }
     }
 }
