1bcfaa282c29fb59469f42541288c90ffe269c0a
René Ghosh <35847968+reneghosh@users.noreply.github.com>
2022-01-25 10:39:32 +0100

    Create hrscript.js

---
+++ personal/hrscript.js
@@ -0,0 +1,98 @@
+"uses strict";
+
+import { Card } from "./cardmaker.js";
+import { add, getColumnNames, getDatabaseRange, makeUUID } from "./sheetserver.js";
+
+const spreadsheetId = "1H3XVO28UsnP9kG2qjOCBmYRvpEQne1UbZTgLF847Hj4";
+var numResults = 0;
+var inputRange;
+
+const log = console.log;
+
+
+
+const appendData = (messages) => {
+    const tr = document.createElement("tr");
+    table.appendChild(tr);
+    for (var i = 0; i < messages.length; i++) {
+        var td = document.createElement("td");
+        tr.appendChild(td);
+        td.innerText = messages[i];
+    }
+}
+
+
+const initDocument = () => {
+    numResults = 0;
+    var table = document.getElementById('table');
+    table.innerHTML = "";
+    const tr = document.createElement("tr");
+    table.appendChild(tr);
+    tr.innerHTML = "<th>Date</th><th>Weight</th><th>BMI</th><th>Local average</th>";
+}
+
+
+const showScriptError = (error) => {
+    document.getElementById("scripterror").innerText = error;
+}
+const showMessage = (message) => {
+    document.getElementById("message").innerText = message;
+}
+
+const listLatestValues = async () => {
+    initDocument();
+    getDatabaseRange(spreadsheetId, "Data");
+    try {
+        let range = await getDatabaseRange(spreadsheetId, "Data");
+        var values = range.slice(-10);
+        if (values.length > 0) {
+            for (let i = 0; i < values.length; i++) {
+                var row = values[i];
+                // print columns A and E, which correspond to indices 0 and 4.
+                appendData([row[1], row[2], row[4], row[5]]);
+            }
+        } else {
+            showScriptError("no data");
+        }
+    } catch (err) {
+        showScriptError('Error: ' + err);
+    };
+}
+
+
+const makeCard = () => {
+    const sendValue = (value) => {
+        // if (isNaN(value)) {
+        //     textInput.showError(`not a number: ${value}`);
+        // } else {
+        addValue(value);
+        textInput.clear();
+        // }
+    }
+    const card = new Card("card");
+    const textInput = card.addTextInput("New weight:", { focused: true });
+    const actionList = card.addActions();
+    const action = actionList.addAction("Add to sheet");
+    action.onclick(() => {
+        sendValue(textInput.getValue());
+    });
+    textInput.onenter(value => {
+        sendValue(value);
+    });
+}
+
+const addValue = async (value) => {
+    await add(spreadsheetId, "Data", {
+        "Date": new Date().toLocaleDateString("fr-FR"),
+        "Weight": value
+    });
+    showMessage("1 row updated");
+    listLatestValues();
+}
+
+
+
+gapiInitialized.then(() => {
+    listLatestValues();
+    makeCard();
+});
